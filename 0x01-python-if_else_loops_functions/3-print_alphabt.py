b6bb436d231f07fbe7ce2358f1a384a86b92cdf2
Dibuji <emekzy10@gmail.com>
2023-06-07 01:20:08 +0000

    changed fstring prints to .format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,7 @@
 def printlowercase():
     for i in range(26):
         if chr(97 + i) != 'q' and chr(97 + i) != 'e':
-            print(chr(97 + i), end=" ")
+            print("{}".format(chr(97 + i)), end="")
 
 
 printlowercase()
