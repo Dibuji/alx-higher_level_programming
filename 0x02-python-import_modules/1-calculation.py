53680c77e5889bbac46595ca6e63ff30215d773d
Dibuji <emekzy10@gmail.com>
2023-06-08 22:23:04 +0000

    edits

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import calculator_1 as calculator
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import calculator_1 as calculator
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, calculator.add(a, b)))
