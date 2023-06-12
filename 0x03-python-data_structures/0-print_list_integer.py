9c23b3f247988c6eb9282aa541c3739d1954e3be
Dibuji <emekzy10@gmail.com>
2023-06-12 17:19:14 +0000

    retrieve at idx

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    total = len(my_list)
-
-    for i in range(total):
-        print("{}".format(int(my_list[i])))
+    for i in my_list:
+        print("{:d}".format(i))
