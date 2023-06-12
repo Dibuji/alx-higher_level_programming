32d309f8732eb038e2f74d3250761306a3aeb55e
Dibuji <emekzy10@gmail.com>
2023-06-12 20:38:35 +0000

    check 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        row_str = ""
         for element in row:
-            print("{:d}".format(element), end=" ")
-        print()
+            row_str += "{:d} ".format(element)
+        print(row_str.strip())
