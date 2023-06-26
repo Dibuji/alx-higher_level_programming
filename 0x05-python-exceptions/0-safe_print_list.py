041c5e060fe68ddea66b94d6fe47f54954d7a99e
Dibuji <emekzy10@gmail.com>
2023-06-26 19:53:06 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    try:
+        while x > 0:
+            print("{}".format(my_list[total]), end="")
+            count += 1
+            x -= 1
+        print("")
+    except IndexError:
+        print("")
+    return total
