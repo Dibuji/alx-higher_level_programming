1458eee1bf03f36c7fe86c54cc711de037b5a8a3
Dibuji <emekzy10@gmail.com>
2023-07-13 02:18:24 +0000

    tasks 4 - 7

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,6 +8,9 @@ def is_same_class(obj, a_class):
     Args:
         obj (any): Object to be checked
         a_class (type): class to be checked against
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
     """
     if isinstance(obj, a_class):
         return True
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,9 @@ def inherits_from(obj, a_class):
         obj (object): Object to be checked
         a_class (type): The class to be checked against
     Returns:
-        True - If obj is an inherited instance of a_class
+        If obj is an inherited instance of a_class - True
         Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
