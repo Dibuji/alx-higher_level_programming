c4005a6eefcc38d48fe73a054507c4287de9e52d
Dibuji <emekzy10@gmail.com>
2023-11-18 04:16:11 +0200

    square args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,5 +11,29 @@ class Square(Rectangle):
     def __str__(self):
         """Defines the str method for the Square Class"""
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
-        )
+                self.id, self.x, self.y, self.width
+                )
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Method that assigns arguments to attributes"""
+        if args:
+            """Update using non-keyworded arguments if they exist"""
+            attributes = ['id', 'size', 'x', 'y']
+            for attr, value in zip(attributes, args):
+                setattr(self, attr, value)
+        
+        elif kwargs:
+            """Update using keyword arguments if no args exist"""
+            for key, value in kwargs.items():
+                setattr(self, key, value)
