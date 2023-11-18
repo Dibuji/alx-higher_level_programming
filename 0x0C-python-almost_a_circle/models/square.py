b301c14052fe6b01735917f2e6415057b11fabf9
Dibuji <emekzy10@gmail.com>
2023-11-18 03:58:14 +0200

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 """Module that defines a Square Class"""
-
-
 from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square Class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Defines the str method for the Square Class"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
